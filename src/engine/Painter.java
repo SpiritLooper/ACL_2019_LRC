d1a71028811f5933cbddc9dcc8dd84e4fb51706d
Julien Claisse <julien.claisse2@etu.univ-lorraine.fr>
2019-10-28 17:16:08 +0100

    ADD engine files

---
+++ src/engine/Painter.java
@@ -0,0 +1,45 @@
+package engine;
+
+import model.game.Game;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Classe permettant de desssiner le jeu à partir de l'image donnée
+ * @author Julien Claisse
+ */
+public class Painter {
+    /**
+     * la taille des cases
+     */
+    protected static final int WIDTH = 800;
+    protected static final int HEIGHT = 600;
+
+    /**
+     * Jeu à dessiner
+     */
+    private Game game;
+
+    public Painter(Game game) {
+        this.game = game;
+    }
+
+    /**
+     * Dessine le jeu
+     * @param im image sur laquelle dessinee
+     */
+    public void draw(BufferedImage im) {
+        Graphics2D crayon = (Graphics2D) im.getGraphics();
+        crayon.setColor(Color.blue);
+        crayon.fillOval(0,0,10,10);
+    }
+
+    public int getWidth() {
+        return WIDTH;
+    }
+
+    public int getHeight() {
+        return HEIGHT;
+    }
+}
