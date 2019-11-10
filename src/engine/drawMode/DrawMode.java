a3957532c2f616fd341c0a1a2fb015fc21b146b5
Julien Claisse <julien.claisse2@etu.univ-lorraine.fr>
2019-11-10 15:43:40 +0100

    ADD Dessin du menu Le menu peut etre affiche avec la touche a

---
+++ src/engine/drawMode/DrawMode.java
@@ -0,0 +1,11 @@
+package engine.drawMode;
+
+import java.awt.*;
+
+/**
+ * Interface permettant de decrire comment dessiner un etat du jeu
+ */
+public interface DrawMode {
+
+    public void draw(Graphics2D g);
+}
